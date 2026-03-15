f22fd62538232f665ab56a040180ccbdad1fae66
Lucas GAIO DOS SANTOS <lucas.gaildossantos@gmail.com>
2026-03-15 16:38:54 +0100

    maj themes

--- frontend/app/middleware/auth.global.ts
+++ frontend/app/middleware/auth.global.ts
@@ -1,5 +1,5 @@
 export default defineNuxtRouteMiddleware(async (to) => {
-  const publicRoutes = ['/', '/login', '/recrutement']
+  const publicRoutes = ['/', '/login', '/recrutement', '/le-geai', '/poles', '/soutenir']
 
   if (publicRoutes.some(route => to.path === route)) {
     return
