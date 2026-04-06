fb86b2ebeee0a687afe31caae3bb835c903d7309
Lucas GAIO DOS SANTOS <lucas.gaildossantos@gmail.com>
2026-04-06 14:52:10 +0200

    maj

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -104,7 +104,6 @@ export default defineNuxtConfig({
   },
 
   routeRules: {
-    '/api/commerce/**': {},
     '/api/**': { proxy: 'http://directus:8055/**' },
     '/': { prerender: true },
     '/le-geai': { prerender: true },
